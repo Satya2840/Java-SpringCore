5294ce5682ae667e4297e16882aca5c2ddec2e8f
Satya <162803395+Satya2840@users.noreply.github.com>
2024-03-14 17:13:15 +0530

    Update and rename AppTest.java to App.java

--- setterInjection/src/main/java/com/satya/setterInjection/list/App.java
+++ setterInjection/src/main/java/com/satya/setterInjection/list/App.java
@@ -3,7 +3,7 @@ package com.satya.setterInjection.list;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AppTest {
+public class App {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/satya/setterInjection/list/context.xml");
