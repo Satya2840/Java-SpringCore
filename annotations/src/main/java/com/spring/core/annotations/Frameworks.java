cb8c524e25522837149265431eaccaccfc078bae
Satya <162803395+Satya2840@users.noreply.github.com>
2024-03-29 13:34:09 +0530

    Update Frameworks.java

--- annotations/src/main/java/com/spring/core/annotations/Frameworks.java
+++ annotations/src/main/java/com/spring/core/annotations/Frameworks.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier
 public class Frameworks implements Course {
 
 	@Override
